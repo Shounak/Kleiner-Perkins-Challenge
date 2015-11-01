e7e46d0e1ed626f34c730b4018722e7a8ff1bd5c
Shounak <shounakkul@gmail.com>
2015-11-01 14:35:35 -0600

    outputs correctly

--- myHashMap.java
+++ myHashMap.java
@@ -1,7 +1,135 @@
-public class myHashMap
+public class myHashMap<T>
 {
-	public myHashMap()
+	private dataNode[] array;
+	private int loadSize = 0;
+	private int mapSize = 0;
+
+	public myHashMap(int size)
 	{
-		System.out.println("Hello there!");
+		array = new dataNode[size];
+		mapSize = size;
+	}
+
+	public boolean set(String key, T value)
+	{
+		// if the map is full, return false
+		if (loadSize >= mapSize)
+			return false;
+
+		dataNode temp = array[findBucket(key)];
+
+		// if this bucket is empty, put the new element there and increment loadSize
+		if (temp == null)
+		{
+			array[findBucket(key)] = new dataNode(key, value);
+			loadSize++;
+		}
+
+		// if the bucket is not empty, iterate to an empty node and put the new element there
+		else
+		{
+			while (temp.next != null)
+				temp = temp.next;
+			temp.next = new dataNode(key, value);
+		}
+		return true;
+	}
+
+	public T get(String key)
+	{
+		dataNode temp = array[findBucket(key)];
+
+		if (temp == null)
+			return null;
+		else
+		{
+			while (key != temp.key)
+			{
+				temp = temp.next;
+				if (temp == null)
+					return null;
+			}
+		}
+
+		return (T)temp.value;
+	}
+
+	public T delete(String key)
+	{
+		dataNode temp = array[findBucket(key)];
+		dataNode tempPrev = temp;
+
+		if (temp == null)
+			return null;
+
+		// if this is the only element in the bucket, I delete it and also decrement load size 
+		if (temp.next == null)
+		{
+			T dataTemp = (T)temp.value;
+			array[findBucket(key)] = null;
+			loadSize--;
+			return dataTemp;
+		}
+
+		else
+		{
+			while (key != temp.key)
+			{
+				tempPrev = temp;
+				temp = temp.next;
+				if (temp == null)
+					return null;
+			}
+		}
+
+		T temporaryData = (T)temp.value;
+		tempPrev.next = temp.next;
+		return temporaryData;
+	}
+
+	// returns the load factor
+	public float load()
+	{
+		return (float)loadSize/(float)mapSize;
+	}
+
+	// returns the bucket that this string key should hash to
+	private int findBucket(String s)
+	{
+		return (s.hashCode() & 0x7FFFFFFF) % mapSize; // By including "& 0x7FFFFFFF", I force the sign bit to always be positive
+	}
+
+	// Visualizes the underlying structure of the hashmap; useful for testing and for showing correct output
+	public void printMap()
+	{
+		for (dataNode d : array)
+		{
+			if (d == null)
+				System.out.println("[]");
+			else
+			{
+				do
+				{
+					System.out.print(" -> ");
+					System.out.print(d.value);
+					d = d.next;
+				} while (d != null);
+				System.out.println();
+			}
+		}
+	}
+
+	// POD type to hold the key,value pairs. Includes a 'next' attribute so it can be used as an element in a LinkedList
+	private class dataNode<T>
+	{
+		String key;
+		T value;
+		dataNode next = null;
+
+		public dataNode(String k, T v)
+		{
+			key = k;
+			value = v;
+		}
 	}
 }
--- myHashMapTest.java
+++ myHashMapTest.java
@@ -2,6 +2,23 @@ public class myHashMapTest
 {
 	public static void main(String[] args)
 	{
-		myHashMap map = new myHashMap();
+		boolean noprint = false;
+		if (args.length != 0 && args[0].toLowerCase() == "noprint")
+			noprint = true;
+
+		myHashMap map = new myHashMap(8);
+		map.set("t", 1);
+		map.set("t", 2);
+		map.set("a", 3);
+		if (!noprint)
+			map.printMap();
+		System.out.println("\n\n");
+		System.out.println("Current load factor: " + map.load());
+		System.out.println("Value at key 'a': " + map.get("a"));
+		map.delete("t");
+		System.out.println("Current load factor after deleting 'a': " + map.load());
+		System.out.println("\n\n");
+		if (!noprint)
+			map.printMap();
 	}
 }
